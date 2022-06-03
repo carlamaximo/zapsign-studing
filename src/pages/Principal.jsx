70a75925767943f85db21bc0e05eb71ac2f8de93
Carla <carladmaximo@gmail.com>
2022-06-03 18:27:57 -0300

    subindo a aplicacao inicial

---
+++ src/pages/Principal.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import GETDocuments from "../components/GETDocuments";
+
+class Principal extends React.Component {
+  render() {
+    return (
+      <div>
+        <GETDocuments />
+      </div>
+    )}
+};
+
+export default Principal;
